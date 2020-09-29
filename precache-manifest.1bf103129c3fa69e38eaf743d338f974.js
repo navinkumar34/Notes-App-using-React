598a68365acac0020c92c545c818c54c27b60b16
Navin Kumar <navinredu@gmail.com>
2020-09-29 19:33:57 +0530

    Updates

---
+++ precache-manifest.1bf103129c3fa69e38eaf743d338f974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a4883425c2472256e860ef4823f466",
+    "url": "/Notes-App-using-React/index.html"
+  },
+  {
+    "revision": "15569bee4aa22120ceb9",
+    "url": "/Notes-App-using-React/static/css/2.2c60062d.chunk.css"
+  },
+  {
+    "revision": "94e7d953de9d181512c2",
+    "url": "/Notes-App-using-React/static/css/main.ddb1a5b5.chunk.css"
+  },
+  {
+    "revision": "15569bee4aa22120ceb9",
+    "url": "/Notes-App-using-React/static/js/2.7838e5cd.chunk.js"
+  },
+  {
+    "revision": "94e7d953de9d181512c2",
+    "url": "/Notes-App-using-React/static/js/main.bcf9a700.chunk.js"
+  },
+  {
+    "revision": "ac2125105b10506998d8",
+    "url": "/Notes-App-using-React/static/js/runtime~main.d658ee9b.js"
+  }
+]);
